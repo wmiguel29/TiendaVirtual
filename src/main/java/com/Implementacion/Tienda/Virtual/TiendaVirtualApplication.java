a7b0a3192cf07a6db710f1a34d85d6ada2ae6664
Miguel Wagner <wmiguel999@gmail.com>
2022-08-31 16:09:20 -0500

    Inicializacion del proyecto

---
+++ src/main/java/com/Implementacion/Tienda/Virtual/TiendaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.Implementacion.Tienda.Virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaVirtualApplication.class, args);
+	}
+
+}
