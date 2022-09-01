e9ed59c9a77e705446032afda1e7ac1d9093ec25
Miguel Wagner <wmiguel999@gmail.com>
2022-09-01 09:51:28 -0500

    Configuracion inicial 2

---
+++ src/test/java/com/implementacion/tienda_virtual/TiendaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.implementacion.tienda_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
