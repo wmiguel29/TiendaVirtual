fb15c1212e58728029ae13a7f026c5c84b773808
Miguel Gaviria Escobar <migaviria@outlook.com>
2022-09-02 09:51:27 -0500

    actualizacion empleado

--- src/main/java/com/implementacion/tienda_virtual/entity/Empleado.java
+++ src/main/java/com/implementacion/tienda_virtual/entity/Empleado.java
@@ -1,4 +1,33 @@
 package com.implementacion.tienda_virtual.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "empleados")
 public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idEmpleados")
+    private long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "salario")
+    private int salario;
+
+    @Column(name = "horarioTrabajo")
+    private String horarioTrabajo;
+
+    @Column(name = "isVendedor")
+    private boolean isVendedor;
 }
